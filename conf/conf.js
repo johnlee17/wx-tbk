8afc5e939a6fb7a9931d2238c2094bef3155f6e5
johnlee <john.ly@foxmail.com>
2017-11-28 00:04:38 +0800

    create

--- conf/conf.js
+++ conf/conf.js
@@ -1,12 +1,12 @@
 module.exports = {
   PROD: {
-    DOMAIN: 'https://m.gdaimama.cn'
+    DOMAIN: 'http://127.0.0.1:3000'
   },
   DEV: {
     DOMAIN: 'http://127.0.0.1:3000'
   },
   WX: {
-    APPID: 'wx9095f54723765908',
-    SECRET: '14767676750954bb7aef6eb28d63a29e'
+    APPID: 'test',
+    SECRET: 'test'
   }
 };
